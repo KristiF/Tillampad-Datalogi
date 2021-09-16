d955b57bb8a79dcdf3f2c9653afc697354174d1e
dundraren <nora.dunder@live.se>
2021-09-16 13:43:24 +0200

    gjort färdigt uppgift 3

--- Labb 3/Dubletter.py
+++ Labb 3/Dubletter.py
@@ -7,4 +7,15 @@ with open("word3.txt", "r", encoding = "utf-8") as svenskfil:
             print(ordet, end = " ")
         else:
             svenska.put(ordet)             # in i sökträdet
+
+engelska = Bintree()
+with open("engelska.txt", "r", encoding = "utf-8") as engelskfil:
+    for rad in engelskfil:
+        for ordet in rad.split():
+            if ordet in engelska:
+                pass
+            elif ordet in svenska:
+                engelska.put(ordet)
+                print(ordet, end=" ")
+
 print("\n")
