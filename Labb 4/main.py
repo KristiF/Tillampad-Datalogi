4aa6f994827f26bc1cdab1e4a801553dbfb63479
dundraren <nora.dunder@live.se>
2021-09-22 12:11:45 +0200

    gjort första versionen

---
+++ Labb 4/main.py
@@ -0,0 +1,27 @@
+from bintreeFile import Bintree
+
+svenska = Bintree()
+gamla = Bintree()
+
+def ordlista():
+    with open("word3.txt", "r", encoding="utf-8") as svenskfil:
+        for rad in svenskfil:
+            ordet = rad.strip()  # Ett trebokstavsord per rad
+            svenska.put(ordet)  # in i sökträdet
+
+#startord = input("Mata in startord: ")
+
+def makeChildren(startord):
+    alfabet = "abcdefghijklmnopqrstuvwxyzåäö" # definierar vilka tecken som existerar i alfabetet
+    gamla.put(startord) # lägger automatiskt in startord som ett dumbarn
+    for bokstav in range(len(startord)): # går igenom varje bokstav i starordet
+        for tkn in alfabet:  # går igenom varje tecken i alfabetet
+            nyttOrd = list(startord) # gör om startordet till en lista där varje bokstav är ett element
+            nyttOrd[bokstav] = tkn #byter ut varje bokstav till varje tecken i alfabetet
+            nyttOrd = "".join(nyttOrd) # sätter ihop alla elementen i listan till en sträng och bildar det nya ordet
+            if nyttOrd in svenska and nyttOrd not in gamla:
+                gamla.put(nyttOrd)
+                print(nyttOrd)
+ordlista()
+makeChildren("söt")
+
