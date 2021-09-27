7c030eade6fd8d8b6a51db5abfc1dc87396eb575
KristiF <francis@kristi.nu>
2021-09-27 12:59:29 +0200

    Påbörja labb 5

---
+++ Labb 5/Main.py
@@ -0,0 +1,6 @@
+def utskrift(lista):
+    if len(lista) > 0:
+        utskrift(lista[1:])
+        print(lista[0])
+
+utskrift([1,2,3,4,5])
---
+++ Labb 5/bintreeFile.py
@@ -0,0 +1,45 @@
+from Node import Node
+class Bintree:
+    def __init__(self):
+        self.root = None
+
+    def put(self, newvalue):
+        self.root = putta(self.root, newvalue)
+
+    def __contains__(self,value):
+        # True om value finns i trädet, False annars
+        return finns(self.root, value)
+
+    def write(self):
+        # Skriver ut trädet i inorder
+        skriv(self.root)
+        print("\n")
+
+
+def putta(root, newvalue):
+    if root is None:
+        return Node(newvalue)
+    if newvalue > root.value:
+        root.right = putta(root.right, newvalue)
+    else:
+        root.left = putta(root.left, newvalue)
+    return root
+
+
+def finns(root, value):
+    if root is None:
+        return False
+    elif root.value == value:
+        return True
+    if value > root.value:
+        return finns(root.right, value)
+    else:
+        return finns(root.left, value)
+
+def skriv(root):
+    if root is None:
+        return
+    skriv(root.left)
+    print(root.value, end=' ')
+    skriv(root.right)
+
